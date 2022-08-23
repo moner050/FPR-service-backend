0127ca354d5dc55ae8a1d59da0a3a17f73ef93d9
moner050 <minhyung0237@gmail.com>
2022-08-23 16:01:06 +0900

    feat: Create MemberController

---
+++ src/main/java/com/fpr/fprservicebackend/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.fpr.fprservicebackend.controller;
+
+import com.fpr.fprservicebackend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private MemberService memberService;
+
+}
