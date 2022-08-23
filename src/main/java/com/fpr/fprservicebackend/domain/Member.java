4b55f8dae7c394a6630e536cd744fc6e6ce120fb
moner050 <minhyung0237@gmail.com>
2022-08-23 16:00:55 +0900

    feat: Create Member Entity

---
+++ src/main/java/com/fpr/fprservicebackend/domain/Member.java
@@ -0,0 +1,31 @@
+package com.fpr.fprservicebackend.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "birthday", nullable = false)
+    private String birthday;
+
+}
