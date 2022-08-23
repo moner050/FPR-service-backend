b411cba2678e896d674987b1a58eedba459959fd
moner050 <minhyung0237@gmail.com>
2022-08-23 17:30:40 +0900

    test: Create MemberServiceTest
    MemberService 테스트 생성
    회원가입 테스트 추가

---
+++ src/test/java/com/fpr/fprservicebackend/service/MemberServiceTest.java
@@ -0,0 +1,37 @@
+package com.fpr.fprservicebackend.service;
+
+import com.fpr.fprservicebackend.domain.Member;
+import com.fpr.fprservicebackend.dto.MemberDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+public class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @Transactional
+    @DisplayName("회원가입 테스트")
+    void 회원가입() throws Exception {
+        // given
+        MemberDto memberDto = new MemberDto();
+        memberDto.setEmail("test@naver.com");
+        memberDto.setPassword("1234");
+        memberDto.setUsername("테스트");
+        memberDto.setBirthday("2022-04-07");
+
+        // when
+        memberService.insertMember(memberDto.toEntity());
+        Member findMember = memberService.getMember("테스트");
+
+        // then
+        Assertions.assertThat(memberDto.getEmail()).isEqualTo(findMember.getEmail());
+    }
+}
