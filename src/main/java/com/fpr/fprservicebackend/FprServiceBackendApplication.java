abd6fa083ad9a304c75b798bbc3461be84bbdd0e
moner050 <minhyung0237@gmail.com>
2022-08-23 15:08:35 +0900

    feat: Create FprServiceBackend Repository Template

---
+++ src/main/java/com/fpr/fprservicebackend/FprServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fpr.fprservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FprServiceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FprServiceBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fpr/fprservicebackend/FprServiceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpr.fprservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FprServiceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
