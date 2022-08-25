9c3d40309fd0343e6668b3bc25f040033f0fccc3
moner050 <minhyung0237@gmail.com>
2022-08-25 21:32:05 +0900

    feat: remove birthday, Add phoneNumber, confirmPassword
    생일 삭제 후 전화번호, 비밀번호 확인 추가.

--- src/main/java/com/fpr/dto/MemberDto.java
+++ src/main/java/com/fpr/dto/MemberDto.java
@@ -4,33 +4,45 @@ import com.fpr.domain.Member;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
-public class MemberDto {
+public class MemberDto{
 
-    private Long memberId;
-
+    @NotNull
     private String username;
 
+    @NotNull
+    @Size(min = 1, max = 200)
     private int age;
 
+    @NotNull
     private String job;
 
+    @NotNull
+    @Email
     private String email;
 
+    @NotNull
     private String password;
 
-    private String birthday;
+    @NotNull
+    private String confirmPassword;
+
+    @NotNull
+    private String phoneNumber;
 
     public Member toEntity(){
         return Member.builder()
                 .username(username)
-                .email(email)
-                .password(password)
                 .age(age)
                 .job(job)
-                .birthday(birthday)
+                .email(email)
+                .password(password)
+                .phoneNumber(phoneNumber)
                 .build();
     }
-
 }
