e71fa6462385a3eae4ead1dd78b038d87a00a315
moner050 <minhyung0237@gmail.com>
2022-08-25 16:20:28 +0900

    feat: Change ProductRepository path
    ProductRepository 경로 수정

--- src/main/java/com/fpr/service/ProductService.java
+++ src/main/java/com/fpr/service/ProductService.java
@@ -1,7 +1,7 @@
 package com.fpr.service;
 
 import com.fpr.domain.Product;
-import com.fpr.persistance.ProductRepository;
+import com.fpr.persistence.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
