a4e856c29e9d5531816f09d0ca64426dfd267657
moner050 <minhyung0237@gmail.com>
2022-08-26 03:53:07 +0900

    feat: change register given
    MemberDto 변경으로 인한 given 수정

--- src/test/java/com/fpr/service/MemberServiceTest.java
+++ src/test/java/com/fpr/service/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.fpr.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fpr.domain.Member;
 import com.fpr.dto.MemberDto;
 import lombok.extern.slf4j.Slf4j;
@@ -7,38 +8,55 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
 @SpringBootTest
 @Slf4j
+@AutoConfigureMockMvc
 public class MemberServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private MemberService memberService;
 
     @Test
     @Transactional
-    @DisplayName("회원가입 테스트")
-    void 회원가입(){
+    @DisplayName("무검증 회원가입 테스트")
+    void 무검증_회원가입(){
         // given
-        MemberDto memberDto = new MemberDto();
-        memberDto.setEmail("test@naver.com");
-        memberDto.setPassword("1234");
-        memberDto.setAge(20);
-        memberDto.setJob("무직");
-        memberDto.setUsername("테스트");
-        memberDto.setPhoneNumber("010-4555-2787");
-
+        MemberDto memberDto = new MemberDto
+                ("홍길동", 20, "무직",
+                 "test@naver.com", "1234",
+                 "1234", "01045525252");
         // when
         memberService.insertMember(memberDto.toEntity());
-        Member findMember = memberService.getMember("테스트");
+        Member findMember = memberService.getMember("홍길동");
 
         log.info("" + findMember.getMemberId());
         log.info("" + findMember.getCreatedAt());
 
         // then
         Assertions.assertThat(memberDto.getEmail()).isEqualTo(findMember.getEmail());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("검증 회원가입 테스트")
+    void 검증_회원가입() throws Exception {
+        // given
+        MemberDto memberDto = new MemberDto
+                ("홍길동", 20, "무직",
+                        "test@naver.com", "1234",
+                        "1234", "01045525252");
+
     }
 }
