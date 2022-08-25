09d92f38b29dad3a9a0af4c98459fa7a587b716a
moner050 <minhyung0237@gmail.com>
2022-08-26 03:39:28 +0900

    feat: Add Temp register
    테스트용 회원가입 추가

--- src/main/java/com/fpr/controller/MemberController.java
+++ src/main/java/com/fpr/controller/MemberController.java
@@ -1,14 +1,25 @@
 package com.fpr.controller;
 
+import com.fpr.dto.MemberDto;
 import com.fpr.service.MemberService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class MemberController {
 
     private final MemberService memberService;
 
+    @PostMapping("/register")
+    public MemberDto register(@RequestBody @Valid MemberDto memberDto){
+
+        return memberDto;
+    }
 }
