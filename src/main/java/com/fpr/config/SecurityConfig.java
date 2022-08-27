cec80c734c1f69f757b38808cbc84f53242eac82
moner050 <minhyung0237@gmail.com>
2022-08-27 12:22:54 +0900

    feat: Add antMatchers
    스웨거 및 각종 api URL 들의 접근권한을 허용

--- src/main/java/com/fpr/config/SecurityConfig.java
+++ src/main/java/com/fpr/config/SecurityConfig.java
@@ -8,10 +8,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private String[] swaggerList = {
+            /* swagger v2 */
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            /* swagger v3 */
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests().antMatchers("/**").permitAll()
+                .authorizeHttpRequests()
+                .antMatchers(swaggerList).permitAll()
+                .antMatchers( "/", "/auth/**", "/product/**", "/cart/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
