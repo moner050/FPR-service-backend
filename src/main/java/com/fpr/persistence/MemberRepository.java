3f0e2774d7e4ddd229cfa51dabe96d6c41c68647
moner050 <minhyung0237@gmail.com>
2022-08-30 20:08:36 +0900

    feat: Create findByEmail, existsByEmail

--- src/main/java/com/fpr/persistence/MemberRepository.java
+++ src/main/java/com/fpr/persistence/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
-    Optional<Member> findByUsername(String username);
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 
 }
