4e227110a78be066b6074929d8b6e33f45e73abc
moner050 <minhyung0237@gmail.com>
2022-08-30 19:02:46 +0900

    feat: Add authority
    권한 컬럼 추가

--- src/main/java/com/fpr/domain/Member.java
+++ src/main/java/com/fpr/domain/Member.java
@@ -14,6 +14,7 @@ import javax.persistence.*;
 public class Member extends BaseTime{
 
     @Id
+    @Column(name = "memberId")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
@@ -35,13 +36,18 @@ public class Member extends BaseTime{
     @Column(name = "phoneNumber", nullable = false)
     private String phoneNumber;
 
+    @Column(name = "authority", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
     @Builder
-    private Member(String username, int age, String job, String email, String password, String phoneNumber) {
+    private Member(String username, int age, String job, String email, String password, String phoneNumber, Authority authority) {
         this.username = username;
         this.age = age;
         this.job = job;
         this.email = email;
         this.password = password;
         this.phoneNumber = phoneNumber;
+        this.authority = authority;
     }
 }
