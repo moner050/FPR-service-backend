eeab5cee32f88c6702854fa44c5a1267761c9a9c
moner050 <minhyung0237@gmail.com>
2022-08-30 20:09:44 +0900

    feat: change name toEntity -> toMember, Add toAuthentication

--- src/main/java/com/fpr/dto/MemberRequestDto.java
+++ src/main/java/com/fpr/dto/MemberRequestDto.java
@@ -1,17 +1,21 @@
 package com.fpr.dto;
 
 import com.fpr.annotation.Tel;
+import com.fpr.domain.Authority;
 import com.fpr.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.validation.constraints.*;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
-public class MemberDto{
+public class MemberRequestDto {
     // 계층간 이동시 데이터가 변조될 위험이 없이 하기위해 Setter 제거
-
     @NotNull
     private String username;
 
@@ -37,14 +41,19 @@ public class MemberDto{
     @Tel
     private String phoneNumber;
 
-    public Member toEntity(){
+    public Member toMember(PasswordEncoder passwordEncoder){
         return Member.builder()
                 .username(username)
                 .age(age)
                 .job(job)
                 .email(email)
-                .password(password)
+                .password(passwordEncoder.encode(password))
                 .phoneNumber(phoneNumber)
+                .authority(Authority.ROLE_USER)
                 .build();
+    }
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
     }
 }
