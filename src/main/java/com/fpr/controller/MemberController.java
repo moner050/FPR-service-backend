aea0d3a992c5bd564e1e2b510dfea1417139b3ca
moner050 <minhyung0237@gmail.com>
2022-08-31 11:08:09 +0900

    feat: restore getMyMemberInfo
    내정보 보기 다시 추가

--- src/main/java/com/fpr/controller/MemberController.java
+++ src/main/java/com/fpr/controller/MemberController.java
@@ -12,6 +12,11 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> getMyMemberInfo() {
+        return ResponseEntity.ok(memberService.getMyInfo());
+    }
+
     @GetMapping("/{email}")
     public ResponseEntity<MemberResponseDto> getMemberInfo(@PathVariable String email) {
         return ResponseEntity.ok(memberService.getMemberInfo(email));
