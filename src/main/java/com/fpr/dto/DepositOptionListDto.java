3f167d944dc3b07e1ff4e249f5203aeb2c97f1c5
moner050 <minhyung0237@gmail.com>
2022-09-01 03:38:40 +0900

    feat: Create DepositOptionListDto

---
+++ src/main/java/com/fpr/dto/DepositOptionListDto.java
@@ -0,0 +1,15 @@
+package com.fpr.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepositOptionListDto {
+
+    private String intr_rate_type;
+    private String intr_rate_type_nm;
+    private String save_trm;
+    private double intr_rate;
+    private double intr_rate2;
+}
