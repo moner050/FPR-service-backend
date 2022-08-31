341d1c8f5590b586441c3300e86ed941bbbf36a9
moner050 <minhyung0237@gmail.com>
2022-09-01 03:52:04 +0900

    feat: Deactivate /me Mapping
    RefreshToken 사용 안함에 따라 비활성화

--- src/main/java/com/fpr/controller/MemberController.java
+++ src/main/java/com/fpr/controller/MemberController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/me")
-    public ResponseEntity<MemberResponseDto> getMyMemberInfo() {
-        return ResponseEntity.ok(memberService.getMyInfo());
-    }
+//    @GetMapping("/me")
+//    public ResponseEntity<MemberResponseDto> getMyMemberInfo() {
+//        return ResponseEntity.ok(memberService.getMyInfo());
+//    }
 
     @GetMapping("/{email}")
     public ResponseEntity<MemberResponseDto> getMemberInfo(@PathVariable String email) {
