603b9e7e986418545acacf8049bbdea97608119a
Ramda_lee <rkdgml357@naver.com>
2022-09-02 01:57:50 +0900

    feat : DepositResponseDto 추가

--- src/main/java/com/fpr/dto/DepositResponseDto.java
+++ src/main/java/com/fpr/dto/DepositResponseDto.java
@@ -1,2 +1,13 @@
-package com.fpr.dto;public class DepositResponseDto {
+package com.fpr.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class DepositResponseDto<T> {
+    int status;
+    T data;
+
+    public DepositResponseDto(T value, T i) {
+    }
 }
