b6fa14faff551172496037294c6411fefe5fad7e
Ramda_lee <rkdgml357@naver.com>
2022-09-02 02:15:54 +0900

    feat : 상품 찾기 기능 추가

--- src/main/java/com/fpr/persistence/SavingRepository.java
+++ src/main/java/com/fpr/persistence/SavingRepository.java
@@ -3,6 +3,9 @@ package com.fpr.persistence;
 import com.fpr.domain.Saving;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SavingRepository extends JpaRepository<Saving, Long> {
 
+    List<Saving> findBykorCoNm(String korCoNm);
 }
