22d69fbf95f720f46e0f25d6c10d44d672d952ed
Ramda_lee <rkdgml357@naver.com>
2022-09-02 02:18:32 +0900

    feat : 상품 찾기 기능 추가

--- src/main/java/com/fpr/service/SavingService.java
+++ src/main/java/com/fpr/service/SavingService.java
@@ -1,2 +1,36 @@
-package com.fpr.service;public class SavingService {
+package com.fpr.service;
+
+import com.fpr.domain.Saving;
+import com.fpr.persistence.SavingRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SavingService {
+
+    private final SavingRepository savingRepository;
+
+    public List<Saving> list(){
+        return savingRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Saving findOne(Long id) {
+        Optional<Saving> product = savingRepository.findById(id);
+        return product.orElseGet(() -> new Saving());
+    }
+
+//    public void recommendProduct(Member member) {
+//        savingRepository.recommend(member.getAge(), member.getJob());
+//    }
+
+    public List<Saving> searchProduct(Saving savingsProduct) {
+        return savingRepository.findBykorCoNm(savingsProduct.getKorCoNm());
+    }
 }
