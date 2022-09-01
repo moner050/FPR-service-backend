78fdd0598fc7ce6466ddcf34d67e3dbf77fd151b
Ramda_lee <rkdgml357@naver.com>
2022-09-02 01:56:49 +0900

    feat : 상품 찾기 기능 함수 생성

--- src/main/java/com/fpr/persistence/DepositRepository.java
+++ src/main/java/com/fpr/persistence/DepositRepository.java
@@ -3,6 +3,9 @@ package com.fpr.persistence;
 import com.fpr.domain.Deposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
 
+    List<Deposit> findBykorCoNm(String korCoNm);
 }
