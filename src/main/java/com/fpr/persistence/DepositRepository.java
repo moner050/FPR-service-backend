3fed6ae82f530e565ff8e010d7ee98b6c642e493
moner050 <minhyung0237@gmail.com>
2022-09-02 14:46:51 +0900

    feat: Create findById
    아이디로 적금 찾기 추가

--- src/main/java/com/fpr/persistence/DepositRepository.java
+++ src/main/java/com/fpr/persistence/DepositRepository.java
@@ -4,8 +4,10 @@ import com.fpr.domain.Deposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
 
-    List<Deposit> findBykorCoNm(String korCoNm);
+    List<Deposit> findByKorCoNmContaining(String korCoNm);
+    Optional<Deposit> findById(Long id);
 }
