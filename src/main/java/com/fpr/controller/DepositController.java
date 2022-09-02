bf654b70ae8d8cff0952ef1105a8b66a80097476
Ramda_lee <rkdgml357@naver.com>
2022-09-02 16:21:21 +0900

    feat : 기능 수정

--- src/main/java/com/fpr/controller/DepositController.java
+++ src/main/java/com/fpr/controller/DepositController.java
@@ -5,8 +5,10 @@ import com.fpr.dto.DepositResponseDto;
 import com.fpr.service.DepositService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,22 +20,21 @@ public class DepositController {
 
     private final DepositService depositService;
 
-    @GetMapping("/deposit")
-    public DepositResponseDto findAll(Model model) {
-        List<Deposit> deposits = depositService.list();
-        model.addAttribute("deposits", deposits);
-        return new DepositResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    @GetMapping("/product/deposit/list")
+    public ResponseEntity<List<Deposit>> depositAll() {
+        List<Deposit> deposits = depositService.searchAll();
+        return new ResponseEntity(deposits, HttpStatus.OK);
     }
 
-    @PostMapping("/product/detail")
-    public DepositResponseDto productDetail(Long id, Model model) {
-        model.addAttribute("deposits", depositService.findOne(id));
-        return new DepositResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    @PostMapping("/product/deposit/detail")
+    public ResponseEntity<Deposit> depositDetail(Long id) {
+        Deposit deposit = depositService.searchOne(id);
+        return new ResponseEntity(deposit, HttpStatus.OK)
     }
 
-    @GetMapping("/product/{id}")
-    public DepositResponseDto searchProduct(Model model, Deposit deposit) {
-        model.addAttribute("deposits", depositService.searchProduct(deposit));
-        return new DepositResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    @PostMapping("/product/deposit/{korCoNm}")
+    public ResponseEntity<List<Deposit>> depositSearch(@PathVariable("korCoNm") String korCoNm) {
+        List<Deposit> deposits = depositService.searchProduct(korCoNm);
+        return new ResponseEntity(deposits, HttpStatus.OK);
     }
 }
