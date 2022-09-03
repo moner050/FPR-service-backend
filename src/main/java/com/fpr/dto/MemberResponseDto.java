99d4960b9ca0c7f1b09b380b8571be52610f1887
moner050 <minhyung0237@gmail.com>
2022-09-03 12:21:06 +0900

    feat: add Member info in MemberResponseDto
    사용자이름, 나이, 직업, 핸드폰번호, 보유제품 목록 추가

--- src/main/java/com/fpr/dto/MemberResponseDto.java
+++ src/main/java/com/fpr/dto/MemberResponseDto.java
@@ -1,17 +1,26 @@
 package com.fpr.dto;
 
 import com.fpr.domain.Member;
+import com.fpr.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberResponseDto {
     private String email;
+    private String username;
+    private int age;
+    private String job;
+    private String phoneNumber;
+    private List<Product> products = new ArrayList<>();
 
     public static MemberResponseDto of(Member member) {
-        return new MemberResponseDto(member.getEmail());
+        return new MemberResponseDto(member.getEmail(), member.getUsername(), member.getAge(), member.getJob(), member.getPhoneNumber(), member.getProducts());
     }
 }
