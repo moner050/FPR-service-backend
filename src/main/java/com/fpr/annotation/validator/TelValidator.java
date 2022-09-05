71918dbe784b048949df4ff9e3f7c85ececa4311
moner050 <minhyung0237@gmail.com>
2022-09-05 21:47:39 +0900

    feat: add TelValidator

---
+++ src/main/java/com/fpr/annotation/validator/TelValidator.java
@@ -0,0 +1,14 @@
+package com.fpr.annotation.validator;
+
+import com.fpr.annotation.Tel;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TelValidator implements ConstraintValidator<Tel, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null) return false;
+        return value.matches("(01[016789])(\\d{3,4})(\\d{4})");
+    }
+}
