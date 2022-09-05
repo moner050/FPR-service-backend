c7c1bd24c917200cda9417682f6ccfaef710c572
moner050 <minhyung0237@gmail.com>
2022-09-06 01:20:01 +0900

    feat: add findAll, findByJoinWayContaining, findByJoinWay, findByPrdtDiv

--- src/main/java/com/fpr/persistence/ProductRepository.java
+++ src/main/java/com/fpr/persistence/ProductRepository.java
@@ -8,6 +8,18 @@ import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // 전체 검색
+    List<Product> findAll();
+
+    // 은행명 검색
     List<Product> findByKorCoNmContaining(String korCoNm);
+    // 은행 아이디로 검색
     Optional<Product> findById(Long id);
+    // 가입방법 검색
+    List<Product> findByJoinWayContaining(String joinWay);
+    // 가입방법 검색
+    List<Product> findByJoinWay(String joinWay);
+
+    // 예적금 전체목록
+    List<Product> findByPrdtDiv(String prdtDiv);
 }
