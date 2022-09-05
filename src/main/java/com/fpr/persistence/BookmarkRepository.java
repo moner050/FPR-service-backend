e476f48b6d512b18e1c6aa5fddea1b4bc56f1738
moner050 <minhyung0237@gmail.com>
2022-09-06 01:16:18 +0900

    feat: delete Cart

--- src/main/java/com/fpr/persistence/BookmarkRepository.java
+++ src/main/java/com/fpr/persistence/BookmarkRepository.java
@@ -1,7 +1,6 @@
 package com.fpr.persistence;
 
 import com.fpr.domain.Bookmark;
-import com.fpr.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
