d8a2fa25634e09151c1c6a88b87ec1f786a9825c
moner050 <minhyung0237@gmail.com>
2022-09-05 21:36:18 +0900

    feat: add findByMemberId

--- src/main/java/com/fpr/persistence/BookmarkRepository.java
+++ src/main/java/com/fpr/persistence/BookmarkRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-    Bookmark findByMemberId(Long memberId);
+    List<Bookmark> findByMemberId(Long memberId);
 }
