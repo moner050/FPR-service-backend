0ec0ce14acc6396c96d66e744bcb9e79b47eb781
moner050 <minhyung0237@gmail.com>
2022-09-06 16:07:32 +0900

    feat: modify findByBookmarkId List -> Optional

--- src/main/java/com/fpr/persistence/BookmarkRepository.java
+++ src/main/java/com/fpr/persistence/BookmarkRepository.java
@@ -4,10 +4,11 @@ import com.fpr.domain.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
-    List<Bookmark> findByMemberId(Long memberId);
+    Optional<Bookmark> findByBookmarkId(Long bookmarkId);
+
 }
