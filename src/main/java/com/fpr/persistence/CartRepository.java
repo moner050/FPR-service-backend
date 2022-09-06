87f57ece888aac945c8820cc46182a7fed19be46
moner050 <minhyung0237@gmail.com>
2022-09-06 16:05:22 +0900

    feat: Add findByCartId

--- src/main/java/com/fpr/persistence/CartRepository.java
+++ src/main/java/com/fpr/persistence/CartRepository.java
@@ -4,12 +4,9 @@ import com.fpr.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-//    Optional<Cart> findByMemberId(Long memberId);
-//    Optional<Cart> findById(Long cartId);
-//    Cart findByMemberId(Long memberId);
+    Optional<Cart> findByCartId(Long cartId);
 }
