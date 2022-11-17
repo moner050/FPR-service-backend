3e84e3d5bea620fe7312250156c66cdb94ee0de4
moner050 <minhyung0237@gmail.com>
2022-11-17 19:21:10 +0900

    Refactor: Modify SwaggerConfig
    스웨거에 JWT토큰을 입력할 수 있게 수정

--- src/main/java/com/fpr/config/SwaggerConfig.java
+++ src/main/java/com/fpr/config/SwaggerConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Tag;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +22,8 @@ public class SwaggerConfig {
     @Bean
     public Docket FprApi(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .groupName("금융상품 추천 API")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.fpr.controller"))
@@ -40,5 +44,20 @@ public class SwaggerConfig {
                 .description("금융상품 추천 API 목록")
                 .version("1.0")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Bearer +accessToken", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Bearer +accessToken", authorizationScopes));
     }
 }
