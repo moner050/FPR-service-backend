ffd5d388037030bf3370c048a2edac4c2180f342
moner050 <minhyung0237@gmail.com>
2022-11-22 21:05:14 +0900

    Feat: Create getMyInfo
    내정보를 보는 URL 추가

--- src/main/java/com/fpr/controller/MemberController.java
+++ src/main/java/com/fpr/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.fpr.controller;
 
 import com.fpr.dto.MemberResponseDto;
 import com.fpr.service.MemberService;
+import com.fpr.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,5 +16,10 @@ public class MemberController {
     @GetMapping("/{id}")
     public ResponseEntity<MemberResponseDto> getMyMemberInfo(@PathVariable Long id) {
         return ResponseEntity.ok(memberService.getMyInfo(id));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponseDto> getMyInfo() {
+        return ResponseEntity.ok(memberService.getMyInfo(SecurityUtil.getCurrentMemberId()));
     }
 }
