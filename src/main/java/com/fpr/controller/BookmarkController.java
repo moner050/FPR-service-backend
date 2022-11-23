701ef733298cc70dbe772b68f8ee0b7bc6c9544a
moner050 <minhyung0237@gmail.com>
2022-11-24 01:58:33 +0900

    feat: Modify BookmarkController Bookmark CRUD

--- src/main/java/com/fpr/controller/BookmarkController.java
+++ src/main/java/com/fpr/controller/BookmarkController.java
@@ -2,10 +2,14 @@ package com.fpr.controller;
 
 import com.fpr.dto.BookmarkResponseDto;
 import com.fpr.service.BookmarkService;
+import com.fpr.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,29 +17,30 @@ public class BookmarkController {
 
     private final BookmarkService bookmarkService;
 
-    // 즐겨찾기 등록
-    @PostMapping("/bookmark/{memberId}/{productId}")
-    public ResponseEntity insertBookmark(@PathVariable Long memberId, @PathVariable Long productId) {
-        bookmarkService.insertBookmark(memberId, productId);
-        return ResponseEntity.ok(HttpStatus.OK);
+    // 즐겨찾기 목록 조회
+    @GetMapping("/bookmarks")
+    public ResponseEntity<List<BookmarkResponseDto>> bookmarkList() {
+        return ResponseEntity.ok(bookmarkService.getBookmarkList(SecurityUtil.getCurrentMemberId()));
+    }
+
+    // 즐겨찾기 담기
+    @PostMapping("/bookmark/{productId}")
+    public ResponseEntity<Objects> insertBookmark(@PathVariable Long productId) {
+        bookmarkService.insertBookmark(SecurityUtil.getCurrentMemberId(), productId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // 즐겨찾기 전체 삭제
-    @DeleteMapping("/bookmark/{bookmarkId}")
-    public void deleteBookmark(@PathVariable Long bookmarkId) {
-        bookmarkService.deleteBookmark(bookmarkId);
+    @DeleteMapping("/bookmarks")
+    public void deleteBookmark() {
+        bookmarkService.deleteAllBookmark(SecurityUtil.getCurrentMemberId());
     }
 
     // 즐겨찾기 단일 삭제
-//    @DeleteMapping("/bookmark/{bookmarkId}/{productId}")
-//    public void deleteOneBookmark(@PathVariable("bookmarkId") Long bookmarkId, @PathVariable("productId") Long productId){
-//        bookmarkService.deleteOneBookmark(bookmarkId, productId);
-//    }
-
-    // 즐겨찾기 목록 조회
-    @GetMapping("/bookmark/{bookmarkId}")
-    public ResponseEntity<BookmarkResponseDto> bookmarkList(@PathVariable("bookmarkId") Long bookmarkId) {
-        return ResponseEntity.ok(bookmarkService.getBookmarkList(bookmarkId));
+    @DeleteMapping("/bookmark/{bookmarkItemId}")
+    public void deleteOneBookmark(@PathVariable("bookmarkItemId") Long bookmarkItemId) {
+        bookmarkService.deleteBookmark(SecurityUtil.getCurrentMemberId(), bookmarkItemId);
     }
+
 
 }
