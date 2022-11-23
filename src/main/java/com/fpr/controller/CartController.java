3573a0a8524005485db829ea2901612b169c8cc6
moner050 <minhyung0237@gmail.com>
2022-11-24 01:03:34 +0900

    Feat: Modify CartController, Add deleteCartItem

--- src/main/java/com/fpr/controller/CartController.java
+++ src/main/java/com/fpr/controller/CartController.java
@@ -2,10 +2,14 @@ package com.fpr.controller;
 
 import com.fpr.dto.CartResponseDto;
 import com.fpr.service.CartService;
+import com.fpr.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,22 +17,28 @@ public class CartController {
 
     private final CartService cartService;
 
+    // 장바구니 목록 조회
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartResponseDto>> cartList() {
+        return ResponseEntity.ok(cartService.getCartList(SecurityUtil.getCurrentMemberId()));
+    }
+
     // 장바구니 담기
-    @PostMapping("/cart/{memberId}/{productId}")
-    public ResponseEntity insertCart(@PathVariable Long memberId, @PathVariable Long productId) {
-        cartService.insertCart(memberId, productId);
-        return ResponseEntity.ok(HttpStatus.OK);
+    @PostMapping("/cart/{productId}")
+    public ResponseEntity<Objects> insertCart(@PathVariable Long productId) {
+        cartService.insertCart(SecurityUtil.getCurrentMemberId(), productId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     // 장바구니 삭제
-    @DeleteMapping("/cart/{cartId}")
-    public void deleteAllCartItem(@PathVariable Long cartId) {
-      cartService.deleteAllCartItem(cartId);
+    @DeleteMapping("/carts")
+    public void deleteAllCartItem() {
+      cartService.deleteAllCartItem(SecurityUtil.getCurrentMemberId());
     }
 
-    // 장바구니 목록 조회
-    @GetMapping("/cart/{cartId}")
-    public ResponseEntity<CartResponseDto> cartList(@PathVariable("cartId") Long cartId) {
-        return ResponseEntity.ok(cartService.getCartList(cartId));
+    // 장바구니 단품 삭제
+    @DeleteMapping("/cart/{cartItemId}")
+    public void deleteCartItem(@PathVariable Long cartItemId) {
+        cartService.deleteCartItem(SecurityUtil.getCurrentMemberId(), cartItemId);
     }
 }
