64cb7384a1e3d8f79d9f88c921ca7122725cdfcc
moner050 <minhyung0237@gmail.com>
2022-11-24 01:59:33 +0900

    feat: Modify BookmarkService Bookmark CRUD

--- src/main/java/com/fpr/service/BookmarkService.java
+++ src/main/java/com/fpr/service/BookmarkService.java
@@ -1,9 +1,11 @@
 package com.fpr.service;
 
 import com.fpr.domain.Bookmark;
+import com.fpr.domain.BookmarkItem;
 import com.fpr.domain.Member;
 import com.fpr.domain.Product;
 import com.fpr.dto.BookmarkResponseDto;
+import com.fpr.persistence.BookmarkItemRepository;
 import com.fpr.persistence.BookmarkRepository;
 import com.fpr.persistence.MemberRepository;
 import com.fpr.persistence.ProductRepository;
@@ -11,65 +13,63 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
+    private final BookmarkItemRepository bookmarkItemRepository;
     private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
 
     // 즐겨찾기 등록
     @Transactional
     public void insertBookmark(Long memberId ,Long productId){
-        Optional<Product> product = productRepository.findById(productId);
-        Optional<Member> member = memberRepository.findById(memberId);
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new RuntimeException("검색된 제품이 없습니다."));
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("검색된 맴버가 없습니다."));
 
-        product.orElseThrow(() -> new RuntimeException("검색된 제품이 없습니다."));
-        member.orElseThrow(() -> new RuntimeException("검색된 맴버가 없습니다."));
+        Bookmark bookmark = bookmarkRepository.findByMemberId(memberId)
+                .orElse(Bookmark.builder().member(member).build());
 
-        Optional<Bookmark> bookmark = bookmarkRepository.findByBookmarkId(memberId);
-        if(!bookmark.isPresent()) {
-            bookmark = Optional.of(new Bookmark());
-        }
+        BookmarkItem bookmarkItem = bookmarkItemRepository.findByBookmarkIdAndProductId(bookmark.getId(), productId)
+                .orElse(BookmarkItem.builder().bookmark(bookmark).product(product).build());
 
-        bookmark.get().setBookmarkId(memberId);
-        bookmark.get().addProduct(product.get());
-
-        BookmarkResponseDto.of(bookmarkRepository.save(bookmark.get()));
+        bookmarkRepository.save(bookmark);
+        bookmarkItemRepository.save(bookmarkItem);
     }
 
-    // 즐겨찾기 삭제
+    // 즐겨찾기 전체 삭제
     @Transactional
-    public void deleteBookmark(Long bookmarkId) {
-        bookmarkRepository.deleteById(bookmarkId);
+    public void deleteAllBookmark(Long bookmarkId) {
+        bookmarkRepository.deleteByMemberId(bookmarkId);
     }
 
     // 즐겨찾기 단일 삭제
-//    @Transactional
-//    public void deleteOneBookmark(Long bookmarkId, Long productId) {
-//        Optional<Product> product = productRepository.findById(productId);
-//        Optional<Member> member = memberRepository.findById(bookmarkId);
-//
-//        product.orElseThrow(() -> new RuntimeException("검색된 제품이 없습니다."));
-//        member.orElseThrow(() -> new RuntimeException("검색된 맴버가 없습니다."));
-//
-//        Optional<Bookmark> bookmark = bookmarkRepository.findByBookmarkId(bookmarkId);
-//
-//        bookmark.get().setBookmarkId(bookmarkId);
-//        bookmark.get().removeProduct(product.get());
-//
-//        BookmarkResponseDto.of(bookmarkRepository.save(bookmark.get()));
-//    }
+    @Transactional
+    public void deleteBookmark(Long memberId, Long bookmarkItemId) {
+        Bookmark bookmark = bookmarkRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new RuntimeException("즐겨찾기가 존재하지 않습니다."));
 
+        BookmarkItem bookmarkItem = bookmarkItemRepository.findByBookmarkIdAndId(bookmark.getId(), bookmarkItemId)
+                .orElseThrow(() -> new RuntimeException("검색된 즐겨찾기 목록이 없습니다."));
+
+        bookmarkItemRepository.delete(bookmarkItem);
+    }
 
     // 즐겨찾기 목록 조회
     @Transactional(readOnly = true)
-    public BookmarkResponseDto getBookmarkList(Long bookmarkId) {
-        return bookmarkRepository.findByBookmarkId(bookmarkId)
-                .map(BookmarkResponseDto::of)
-                .orElseThrow(() -> new RuntimeException("즐겨찾기 목록이 없습니다."));
+    public List<BookmarkResponseDto> getBookmarkList(Long memberId) {
+        Bookmark bookmark = bookmarkRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new RuntimeException("즐겨찾기가 존재하지 않습니다."));
+
+        List<Product> productList = bookmarkItemRepository.findAllByBookmarkId(bookmark.getId())
+                .stream().map(BookmarkItem::getProduct).collect(Collectors.toList());
+
+        return productList.stream().map(BookmarkResponseDto::of).collect(Collectors.toList());
     }
 }
