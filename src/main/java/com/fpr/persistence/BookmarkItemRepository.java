08c0217a861447e5c2f26ed049617b6c03f46e76
moner050 <minhyung0237@gmail.com>
2022-11-24 01:57:33 +0900

    feat: Create BookmarkItemRepository

---
+++ src/main/java/com/fpr/persistence/BookmarkItemRepository.java
@@ -0,0 +1,18 @@
+package com.fpr.persistence;
+
+import com.fpr.domain.BookmarkItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookmarkItemRepository extends JpaRepository<BookmarkItem, Long> {
+
+    Optional<BookmarkItem> findById(Long id);
+    Optional<BookmarkItem> findByBookmarkIdAndId(Long BookmarkId, Long BookmarkItemId);
+    Optional<BookmarkItem> findByBookmarkIdAndProductId(Long BookmarkId, Long productId);
+
+    List<BookmarkItem> findAllByBookmarkId(Long BookmarkId);
+}
