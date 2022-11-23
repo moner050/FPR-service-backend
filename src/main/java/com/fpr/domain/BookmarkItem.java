ba2b5c57031edcb5722df96589fb24549fb46861
moner050 <minhyung0237@gmail.com>
2022-11-24 01:57:16 +0900

    feat: Create BookmarkItem

---
+++ src/main/java/com/fpr/domain/BookmarkItem.java
@@ -0,0 +1,34 @@
+package com.fpr.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "bookmark_item")
+public class BookmarkItem extends BaseTime{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bookmark_id")
+    private Bookmark bookmark;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Builder
+    private BookmarkItem(Bookmark bookmark, Product product) {
+        this.bookmark = bookmark;
+        this.product = product;
+    }
+}
