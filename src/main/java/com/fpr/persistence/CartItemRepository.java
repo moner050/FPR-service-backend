eabfc1049cf1ef555c80e5f02f5ac451655cc598
moner050 <minhyung0237@gmail.com>
2022-11-23 23:50:17 +0900

    Feat: Modify CartItemRepository

--- src/main/java/com/fpr/persistence/CartItemRepository.java
+++ src/main/java/com/fpr/persistence/CartItemRepository.java
@@ -4,7 +4,15 @@ import com.fpr.domain.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+    Optional<CartItem> findById(Long id);
+    Optional<CartItem> findByCartIdAndId(Long cartId, Long cartItemId);
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+
+    List<CartItem> findAllByCartId(Long cartId);
 }
